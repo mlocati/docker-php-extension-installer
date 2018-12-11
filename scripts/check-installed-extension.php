5b174581ee8af11c7aa7f16a5d5026b0897517e3
Michele Locati <michele@locati.it>
2018-12-11 15:48:50 +0100

    Remove not passing extensions, test fewer extensions, fix installed extensions check

---
+++ scripts/check-installed-extension.php
@@ -0,0 +1,21 @@
+<?php
+
+$extension = isset($argv[1]) ? trim($argv[1]) : '';
+
+if ($extension === '') {
+    fprintf(STDERR, "Missing module handle.\n");
+    exit(1);
+}
+$nameMap = array(
+    'opcache' => 'Zend OPcache',
+);
+$extensionLowerCase = strtolower($extension);
+if (isset($nameMap[$extensionLowerCase])) {
+    $extension = $nameMap[$extensionLowerCase];
+}
+
+if (extension_loaded($extension)) {
+    exit(0);
+}
+fprintf(STDERR, sprintf("Extension not loaded: %s\n", $extension));
+exit(1);
