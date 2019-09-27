25a58a500fab57616c06cff1a00e4ed199c3fa6b
Michele Locati <michele@locati.it>
2019-09-27 17:29:02 +0200

    Add support to UOPZ
    uopz disables exit() by default, so we can't assume that exit()
    will actually quit a PHP script

--- scripts/check-installed-extension.php
+++ scripts/check-installed-extension.php
@@ -1,22 +1,24 @@
 <?php
 
 $extension = isset($argv[1]) ? trim($argv[1]) : '';
+$rc = 1;
 
 if ($extension === '') {
     fprintf(STDERR, "Missing module handle.\n");
-    exit(1);
-}
-$nameMap = array(
-    'opcache' => 'Zend OPcache',
-);
-$extensionLowerCase = strtolower($extension);
-if (isset($nameMap[$extensionLowerCase])) {
-    $extension = $nameMap[$extensionLowerCase];
+} else {
+    $nameMap = array(
+        'opcache' => 'Zend OPcache',
+    );
+    $extensionLowerCase = strtolower($extension);
+    if (isset($nameMap[$extensionLowerCase])) {
+        $extension = $nameMap[$extensionLowerCase];
+    }
+    if (!extension_loaded($extension)) {
+        fprintf(STDERR, sprintf("Extension not loaded: %s\n", $extension));
+    } else {
+        fprintf(STDOUT, sprintf("Extension correctly loaded: %s\n", $extension));
+        $rc = 0;
+    }
 }
 
-if (extension_loaded($extension)) {
-    fprintf(STDOUT, sprintf("Extension correctly loaded: %s\n", $extension));
-    exit(0);
-}
-fprintf(STDERR, sprintf("Extension not loaded: %s\n", $extension));
-exit(1);
+exit($rc);
