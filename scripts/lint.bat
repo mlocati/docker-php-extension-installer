bff8402fa99cc58e1b46ffd8860e661a55b8063f
Michele Locati <michele@locati.it>
2019-12-20 18:44:45 +0100

    Simplify linting

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -11,12 +11,8 @@ set SRC_DIR=%CD%
 docker build -t docker-php-extension-installer-shfmt:latest -f scripts\Dockerfile-shfmt -q .
 if errorlevel 1 goto :err
 
-call :fix install-php-extensions
-call :fix scripts/common
-call :fix scripts/lint
-call :fix scripts/travisci-test-extensions
-call :fix scripts/travisci-update-readme
-call :fix scripts/update-readme
+docker run --rm -v "%SRC_DIR%:/src" -w /src docker-php-extension-installer-shfmt:latest ./scripts/invoke-shfmt fix
+if errorlevel 1 goto :err
 goto :eof
 
 :no-docker
@@ -26,9 +22,3 @@ goto :eof
 :err
 echo ERROR! >&2
 goto :eof
-
-:fix
-echo|set /p="Fixing %1... "
-docker run --rm -v "%SRC_DIR%:/src" -w /src docker-php-extension-installer-shfmt:latest shfmt -s -ln posix -i 0 -ci -kp -w %1
-if not errorlevel 1 echo done.
-exit /b 0
