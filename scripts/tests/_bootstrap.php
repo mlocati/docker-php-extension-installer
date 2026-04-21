c68071012f267233d083eee12fb4020064922a2f
Michele Locati <michele@locati.it>
2026-04-21 10:43:32 +0200

    [temp] Test AVIF support for imagick
    Test: imagick

--- scripts/tests/_bootstrap.php
+++ scripts/tests/_bootstrap.php
@@ -16,3 +16,31 @@ set_error_handler(
     },
     -1
 );
+
+/**
+ * @example ['alpine', '3.18']
+ * @example ['debian', 13]
+ */
+function inspectOS()
+{
+    $osRelease = file_get_contents('/etc/os-release');
+    if (!preg_match('/^ID=(.+)$/m', $osRelease, $matches)) {
+        fwrite(STDERR, "Unable to determine OS from /etc/os-release\n");
+        exit(1);
+    }
+    $id = trim($matches[1], '"');
+    if (!preg_match('/^VERSION_ID=(.+)$/m', $osRelease, $matches)) {
+        fwrite(STDERR, "Unable to determine OS version from /etc/os-release\n");
+        exit(1);
+    }
+    $versionID = trim($matches[1], '"');
+    switch ($id) {
+        case 'alpine':
+            return [$id, $versionID];
+        case 'debian':
+            return [$id, (int) $versionID];
+        default:
+            fwrite(STDERR, "Unsupported OS: {$id}\n");
+            exit(1);
+    }
+}
